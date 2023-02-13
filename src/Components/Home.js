d7d1fbc5f20901d118b25bd5fde4a34b396fe1ec
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-02-13 22:15:34 +0000

    position elements z-index WIP

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,10 +2,14 @@ import React from "react";
 
 function Home() {
     return (
-          <div className="Home text-white">
-            <h1>TEST - H1 - can you read this??</h1>
-            <p>TEST - ABOUT SECTION</p>
+        <section className="home mt-5 pt-5">
+          <div className="home text-white ">
+            <div className="container bg-transparent" id="about">
+              <h1>TEST - H1 - can you read this??</h1>
+              <p>TEST - ABOUT SECTION</p>
+            </div>
           </div>
+        </section>
     );
   }
 
