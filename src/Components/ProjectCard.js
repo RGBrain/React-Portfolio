cba39c8208b3961eae2ef2d7f0c7fff0acaaef0b
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-02-15 01:51:11 +0000

    finish project cards

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -3,12 +3,12 @@ import React from "react";
 function ProjectCard(props) {
     {console.log(props.image)}
     return (
-        <div className="col-md-4">
+        <div className="col-lg-4 col-md-6 col-sm-12">
             <div className="card">
                 <img src={props.image} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h3 className="text-white">{props.title}</h3>
-                    <p className="card-text text-white">{props.info}</p>
+                    <p id="card-info" className="card-text text-white">{props.info}</p>
                     <div className="button-area">
                         <a href={props.gitHub} className="btn-lg btn btn-primary" id="gitBtn">GitHub</a>
                         { props.liveUrl !== "not live" ? <a href={props.liveUrl} className="btn-lg btn btn-primary" id="liveBtn">Live</a> : <a href={props.liveUrl} className="disabled btn-lg btn btn-primary" id="liveBtn">CLA</a>
