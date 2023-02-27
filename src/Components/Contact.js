3145062b0f4f06d4c1ec3429adbe6396442b2e19
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-02-27 20:43:40 +0000

    clear contact form upon submission

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 import emailjs from 'emailjs-com';
 
 function Contact() {
 
     const form = useRef();
+    const [user_name, setUser_name] = useState('');
+    const [user_email, setUser_Email] = useState('');
+    const [message, setMessage] = useState('')
+
+    const handleNameChange = (event) => {
+        setUser_name(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+        setUser_Email(event.target.value);
+    };
+
+    const handleMsgChange = (event) => {
+        setMessage(event.target.value);
+    };
+
 
     const sendEmail = (e) => {
     e.preventDefault();
@@ -13,10 +29,14 @@ function Contact() {
       .then((result) => {
           console.log(result.text);
           alert("Email sent.");
+          setUser_name('');
+          setUser_Email('');
+          setMessage('');
       }, (error) => {
           console.log(error.text);
           alert("We have encountered a problem", error);
       }
+        // Clear input values after form submitted
       );
   };
 
@@ -31,15 +51,41 @@ function Contact() {
                         <div className="text-center border rounded" style={{height: "70px"}} id="contact-heading">
                             <h2 className="text-white mt-3">Get in Touch</h2>
                         </div>
-                        <input type="name" name="user_name" className="form-control mb-1 mt-3 text-white" style={{height: "40px"}} id="name" placeholder="Name"/>
+                        <input 
+                            type="name" 
+                            name="user_name" 
+                            className="form-control mb-1 mt-3 text-white" 
+                            style={{height: "40px"}} 
+                            id="name" 
+                            placeholder="Name"
+                            value={user_name}
+                            onChange={handleNameChange}
+                        />
 
-                        <input type="email" for="email" name="user_email" className="form-control mb-1 mt-3 text-white" id="email" placeholder="Email" />
+                        <input 
+                            type="email" 
+                            for="email" 
+                            name="user_email" 
+                            className="form-control mb-1 mt-3 text-white" 
+                            id="email" 
+                            placeholder="Email"
+                            value={user_email}
+                            onChange={handleEmailChange}
+                        />
                         
-                        <textarea id="query" for="email_body" name="message" placeholder="Message" className="form-control mb-1 mt-3 text-white" style={{height: "140px"}}></textarea>
+                        <textarea 
+                            id="query" 
+                            for="email_body" 
+                            name="message" 
+                            placeholder="Message" 
+                            value={message}
+                            className="form-control mb-1 mt-3 text-white" 
+                            style={{height: "140px"}}
+                            onChange={handleMsgChange}>
+                        </textarea>
 
                         <div className="text-center mb-4 mt-3 bg-transparent text-white ">
                             <button type="submit" className="btn btn-secondary glow-on-hover">Submit</button>
-
                         </div>
 
                     </form>
