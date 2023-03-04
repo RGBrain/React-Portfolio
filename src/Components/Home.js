7f8c27d26c7ecdc821db211f26014af4e01b81c0
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-03-04 19:41:15 +0000

    moved images folder to src

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
-import profilePic from '../../public/images/portrait-pic5'
 
 function Home() {
 
@@ -14,7 +13,7 @@ function Home() {
           <div className="home text-white" id="intro">
             <div className="container border rounded mb-5" id="about">
               <h1 className="mb-4">Portfolio</h1>
-              <img className="float-end border rounded" id="portrait" src={profilePic} alt="Richard Brain Portrait" height={300}></img>
+              <img className="float-end border rounded" id="portrait" src="/images/portrait-pic5.jpeg" alt="Richard Brain Portrait" height={300}></img>
               <p className="lead">Hi I'm Richard, thanks for checking-out my portfolio.</p>
               <p className="lead">I'm passionate about web development, and life's too short to not do what you love!  Now with my Front-End Web Development & UI/UX certificate in-hand (University of Birmingham), I'm super-excited to see where this adventure takes me!</p>
               <p className="lead">My background is in business development for software companies.  As such, I have many transferrable skills to take into my new career (sales, marketing, communications, project management etc).  I also have a good understanding of Marketing Technology, having worked with companies such as M&C Saatchi, Acxiom and Push Group.</p>
