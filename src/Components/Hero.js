0c50ef5efaca676767bb607b621920f34d141ea9
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-03-07 14:11:38 +0000

    add img background for mob users

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,12 +1,18 @@
 import React from "react";
 import retroBanner from '../Assets/retroBanner.mp4'
+import retroBannerImg from '../Assets/retroBannerMob.png'
 
 function Hero() {
     return (
           <div className="hero-wrapper">
-          <div className="main-hero">
-            <video src={retroBanner} autoPlay loop muted playsInline/>
-          </div>
+            {/* Video background for larger screens */}
+            <div className="main-hero" id="video-background">
+              <video src={retroBanner} autoPlay loop muted playsInline/>
+            </div>
+            {/* Image background for mobiles */}
+            <div className="main-hero" >
+              <img src={retroBannerImg} id="image-background" alt="Futuristic background"/>
+            </div>
           </div>
     );
   }
